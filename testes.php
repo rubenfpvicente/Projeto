e27044bdec16698ea0022ab69a6413faf9ec3c0d
RubyVicente <104641530+rubenfpvicente@users.noreply.github.com>
2022-05-13 15:45:17 +0000

    Updated

--- testes.php
+++ testes.php
@@ -31,21 +31,3 @@ $agencia->adicionarModelo($modelo);
 
 
 $agencia->exportar();
-
-
-$morada = new Morada('Rua principal', '1A', '9500-123', 'Ponta Delgada', 
-    'Portugal');
-
-echo $morada->exportar();
-
-$morada2 = new Morada('','','','','');
-var_dump($morada2);
-$morada2->importar($morada->exportar());
-var_dump($morada2);
-
-$modelo = new Modelo('Joana', $morada, '123', '99999', 'Masculino', 'Francesa', 
-    1.89, 1, 2, 3);
-
-$agencia = new Agencia();
-$agencia->adicionarModelo($modelo);
-$agencia->exportar();
